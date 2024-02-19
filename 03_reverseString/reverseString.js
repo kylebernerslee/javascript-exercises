c5c2b6f67cc1cb93f0699f3432bccc586be80d3b
kylebernerslee <159580020+kylebernerslee@users.noreply.github.com>
2024-02-19 22:24:23 +0100

    Finished Exercise 3
    Finished exercise 3 reverseString.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let Original = string;
+    let Reverse = new Array();
 
+    for(i = 0; i <= Original.length; i++)
+    {
+        Reverse.push(Original[Original.length - i]);
+    }
+    return Reverse.join("");
 };
 
 // Do not edit below this line
