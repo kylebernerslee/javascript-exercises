6624c45b46b0036cfe99cdd99e72b9069d43d645
kylebernerslee <159580020+kylebernerslee@users.noreply.github.com>
2024-02-19 22:01:10 +0100

    Started working on JavasScript Exercises
    Finished Exercise 1 and 2 to get it started.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, number) {
+    let finalString = "";
 
+    if(number < 0) return 'ERROR';
+    for(i = 0; i < number; i++)
+    {
+        finalString += string;
+    }
+
+    return finalString;
 };
 
 // Do not edit below this line
