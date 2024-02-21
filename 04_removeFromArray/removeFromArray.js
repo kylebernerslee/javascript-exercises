ca97cd96af3f84c26594bfdb0d430a01bdd121ed
kylebernerslee <159580020+kylebernerslee@users.noreply.github.com>
2024-02-21 15:36:35 +0100

    Finished exercise 4, what a whirlwind.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(source, ...pointer) {
+    let endArray = new Array();
 
+        source.forEach(item => {
+            if(!pointer.includes(item))     
+                endArray.push(item);
+        });
+    return endArray;
 };
 
 // Do not edit below this line
