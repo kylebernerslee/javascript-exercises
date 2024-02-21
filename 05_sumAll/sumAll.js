f5c8578abefdc1b1e10cca47ac13fda5e42ce0b4
kylebernerslee <159580020+kylebernerslee@users.noreply.github.com>
2024-02-21 19:59:21 +0100

    Finished exercise 05 sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,34 @@
-const sumAll = function() {
+const sumAll = function(numberFrom, numberTo) {
+    let sum = 0;
+    let lowNumber;
+    let highNumber;
 
+    if(numberFrom < 0 || numberTo < 0)
+        return "ERROR";
+
+    if(!(typeof numberTo === 'number') || !(typeof numberFrom === 'number'))
+        return "ERROR";
+
+    if(Array.isArray(numberFrom) || Array.isArray(numberTo))
+        return "ERROR";    
+
+    if(numberFrom < numberTo)
+    {
+        lowNumber = numberFrom;
+        highNumber = numberTo;
+    }
+    else
+    {
+        lowNumber = numberTo;
+        highNumber = numberFrom;
+    }
+
+    for(i = lowNumber; i < (highNumber+1); i++)
+    {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
