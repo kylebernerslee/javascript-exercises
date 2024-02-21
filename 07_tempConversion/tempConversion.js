34ed3e647cbf746e93c310395ac1de4c688fe5a1
kylebernerslee <159580020+kylebernerslee@users.noreply.github.com>
2024-02-21 20:43:00 +0100

    Finished up to exercise 07.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(number) {
+  return (Math.round(((number-32)/1.8) * 10) / 10);
+  //return Math.round((number-32)/1.8);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(number) {
+  return (Math.round((number * 1.8 + 32) * 10) / 10);
+  //return Math.round(number * 1.8 + 32);
 };
 
 // Do not edit below this line
