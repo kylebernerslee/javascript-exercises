90be5c6697b5afd76e9c6c1335ab13aee7701e93
kylebernerslee <159580020+kylebernerslee@users.noreply.github.com>
2024-03-07 22:01:08 +0100

    Finished Palindrome Assignment

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (palindrom) {
 
+    const palindrom_new = palindrom.toLowerCase().replace(/[^a-z0-9]/g, "");
+    const palinArray = Array.from(palindrom_new);
+    const reverse = new Array();
+    for(i = 0; i < palinArray.length; i++) {
+        reverse[palinArray.length - i - 1] = palinArray[i];
+    }
+    const palinReverse = reverse.reduce((a, b) => a += b);
+    if(palindrom_new == palinReverse)
+        return true;
+    else
+        return false;
 };
 
 // Do not edit below this line
