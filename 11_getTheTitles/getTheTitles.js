cef18d72c4570b7f5d79b4b346acfd5f7fc4e276
kylebernerslee <159580020+kylebernerslee@users.noreply.github.com>
2024-03-09 14:12:15 +0100

    Finished getTheTitles.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let titleArray = []; 
+    books.forEach((book, i) => titleArray[i] = book.title);
+    return titleArray;
 };
 
 // Do not edit below this line
