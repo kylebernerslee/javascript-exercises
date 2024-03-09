2e6d1d5f03e0831ec159f15d6fd2cc87df7e83ce
kylebernerslee <159580020+kylebernerslee@users.noreply.github.com>
2024-03-09 20:26:03 +0100

    Finished the findTheOldest Assignment.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.forEach((person) => { 
+        if(person.yearOfDeath === undefined)
+            person.yearOfDeath = new Date().getFullYear();
+        });
+    people.forEach((person) => person.age = person.yearOfDeath - person.yearOfBirth);
+    console.log(people);
+    const oldestPerson = people.reduce((initValue, currentValue) => {
+        if(initValue.age < currentValue.age)
+            initValue = currentValue        
+        return initValue;
+    }) 
+    return oldestPerson;
 };
+
+//name: "Carly",
+//yearOfBirth: 1942,
+//yearOfDeath: 1970,
 
 // Do not edit below this line
 module.exports = findTheOldest;
